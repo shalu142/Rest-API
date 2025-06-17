196226f8448f0e022802e31ebb8e0d8d64fa7fb5
Shalu Baloda <137140429+shalu142@users.noreply.github.com>
2025-06-17 16:23:03 +0530

    Create JsonObject.java

---
+++ JsonObject.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+class JsonObject {
+
+    Object get(String temp) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    JsonObject getAsJsonObject(String main) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
