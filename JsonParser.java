af796524b420d392748a78b77ad7ba59c9bc415d
Shalu Baloda <137140429+shalu142@users.noreply.github.com>
2025-06-17 16:41:32 +0530

    Update JsonParser.java

--- JsonParser.java
+++ JsonParser.java
@@ -1,13 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-
-class JsonParser {
-
-    static Object parseString(String toString) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-}
